b999178adfea8018d8a9bcaa00d87250e5a2f6e0
KolKemboi <kolkemboi@gmail.com>
2025-05-28 07:55:12 +0300

    autocomplete added

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,4 +1,3 @@
-
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
@@ -6,44 +5,43 @@ return {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
+    {
+      "saghen/blink.nvim",
+      config = function()
+        require("blink").setup()  -- initialize blink after cmp is loaded
+      end,
+    },
   },
   config = function()
     local cmp = require("cmp")
 
     cmp.setup({
       snippet = {
-        -- No snippet engine configured; if you want one, add here
         expand = function(args)
-          -- For example: require('luasnip').lsp_expand(args.body)
+          -- Add a snippet engine if needed (like luasnip)
         end,
       },
-      mapping = {
-        -- Tab to autocomplete common prefix (like autopopulate)
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            -- insert common prefix of items without confirming selection
-            cmp.confirm({ select = false, behavior = cmp.ConfirmBehavior.Insert })
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
+mapping = cmp.mapping.preset.insert({
+  ["<Tab>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_next_item()
+    elseif vim.fn.col('.') ~= 1 and vim.fn.getline('.'):sub(vim.fn.col('.') - 1, vim.fn.col('.') - 1):match("%s") == nil then
+      cmp.complete()
+    else
+      fallback()
+    end
+  end, { "i", "s" }),
 
-        -- Ctrl-Tab to move down the list
-        ["<C-Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+  ["<S-Tab>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_prev_item()
+    else
+      fallback()
+    end
+  end, { "i", "s" }),
 
-        -- Shift-Ctrl-Tab to move up the list
-        ["<S-C-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-
-        -- Disable Enter key confirmation
-        ["<CR>"] = cmp.mapping(function(fallback)
-          -- Do nothing on Enter when menu visible
-          if cmp.visible() then
-            fallback()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      },
+  ["<CR>"] = cmp.mapping.confirm({ select = true }),
+}),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "buffer" },
